528ad5b33cef679525fe5c4a4bd44c75ad4738b7
Raphael <raphaelmiyoyo14@gmail.com>
2021-05-24 13:05:09 +0300

    Update port.

--- ussd-test.js
+++ ussd-test.js
@@ -1,45 +1,46 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const UssdMenu = require("ussd-menu-builder");
 const menu = new UssdMenu();
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post("/ussd", (req, res) => {
-  // Read the variables sent via POST from our API
-  const { sessionId, serviceCode, phoneNumber, text } = req.body;
-
-  let response = "";
-
-  menu.state("personDetails", {
-    run: (args) => {
-      let firstName = "";
-      let lastName = "";
-      let age = "";
-      menu.con("Welcome please provide your first name,last name and age");
-      menu.con("Enter first name");
-      firstName = menu.val;
-      menu.session.set("firstName", firstName).then(() => {
-        menu.con("Enter your last name");
-        lastName = menu.val;
-        menu.session.set("lastName", lastName).then(() => {
-          menu.con("Enter your age");
-          age = menu.val;
-          menu.session.set("age", age);
-        });
+menu.state("personDetails", {
+  run: (args) => {
+    let firstName = "";
+    let lastName = "";
+    let age = "";
+    menu.con("welcome please provide your 1st name,last name and age");
+    menu.con("enter first name");
+    firstName = menu.val;
+    menu.session.set("firstName", firstName).then(() => {
+      menu.con("Enter your last name");
+      lastName = menu.val;
+      menu.session.set("lastName", lastName).then(() => {
+        menu.con("Enter your age");
+        age = menu.val;
+        menu.session.set("age", age);
       });
-      if (age > 50) {
-        menu.end("Please retire");
-      }
-      menu.end(
-        `Your name is ${firstName} ${lastName} age ${age} continue working`
-      );
-    },
+    });
+    if (age > 50) {
+      menu.end("please retire");
+    }
+    menu.end(
+      `your name is ${firstName} ${lastName} age ${age} continue working`
+    );
+  },
+});
+// Registering USSD handler with Express
+
+app.post("/", function (req, res) {
+  menu.run(req.body, (ussdResult) => {
+    res.send(ussdResult);
   });
 });
-
-app.listen(3000,()=>{
-  console.log('listening on port 3000 ')})
+// listening
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
